f9e49e855429ce413d88a4f99b86ed28e54be3ef
TheFuZeeXD <forltartem@gmail.com>
2025-06-23 03:49:56 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './index.css'
+import Banner from "/image/wallpaper.dark.jpg"
+import BannerLight from "/image/wallpaper.light.jpg"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Code from "./components/code/Code"
+import { useEffect } from "react";
+
+// Created by TheFuZeeXD
+// 2025 © TheFuZeeXD All rights reserved.
+
+export default function App()
+ { 
+    useEffect(() => {
+    let preloader = document.getElementById("preloader");
+    preloader.classList.add("LoadHidden");
+    let bodycompleteload = document.querySelector("body");
+    bodycompleteload.classList.add("LoadHidden");
+    }, []);
+
+    return (
+        <>
+        <div id="preloader">
+          <div className="preloader-container">
+            <div className="text-glow"></div>
+            <div className="fz-text">
+                <span className="f-letter" data-char="F">F</span>
+                <span className="z-letter" data-char="Z">Z</span>
+            </div>
+        </div>
+          <div className="scene">
+         
+        </div>
+          <div className="loader"></div>
+      </div>
+
+        <img id="Banner-image" src={localStorage.getItem("Theme-color") == "Hight" ? Banner : BannerLight}></img>
+        <div className='content-body'>
+            <Header />
+            <Code />
+            <Footer />
+        </div>
+        </>
+    )
+}
+
+export const Background = document.querySelector("#Banner-image");
