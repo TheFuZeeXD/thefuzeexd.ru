0753fb435f22aab13b0a2edb3d50cc8c21f997ec
TheFuZeeXD <forltartem@gmail.com>
2025-06-23 03:52:42 +0300

    Create Header.jsx

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+import Logo from "/image/logo.jpg"
+
+export default function Header() {
+    return (
+        <header>
+            <div className="LogoType">
+            <img src={Logo}></img>
+            <h1>TheFuZee<span>XD</span></h1>
+            </div>
+        </header>
+    )
+}
