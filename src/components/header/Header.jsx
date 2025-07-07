d4bad29a7903df03ea8bcb3c66dbbcba08d9c772
fuzeexd <fuzeexd@thefuzeexd.ru>
2025-07-07 10:15:25 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,10 @@ import Logo from "/image/logo.jpg"
 export default function Header() {
     return (
         <header>
-            <div className="LogoType">
-            <img src={Logo}></img>
-            <h1>TheFuZee<span>XD</span></h1>
+            <div className="LogoType" onClick={() => { location.reload() }}>
+                <img src={Logo}></img>
+                <h1>TheFuZee<span>XD</span></h1>
             </div>
-        </header>
+        </header >
     )
 }
