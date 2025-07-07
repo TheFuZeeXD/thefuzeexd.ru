7c0b5dd28156f3269f3bc0091c28c512a6de0475
fuzeexd <fuzeexd@thefuzeexd.ru>
2025-07-07 10:14:34 +0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,9 @@
 import "./footer.css"
 import Sun from "/image/svg/sun-icon.svg"
-import Banner from "/image/wallpaper.dark.jpg"
-import BannerLight from "/image/wallpaper.light.jpg"
 import Mon from "/image/svg/moon-icon.svg"
-import configSettings from "../../assets/client.ts"
+import { configSettings } from "../../assets/client.ts"
 import { useState, useEffect } from "react";
-import { Background } from "../../App.jsx";
+import LanguagePack from "../../assets/lang.ts";
 
 // Created by TheFuZeeXD
 // 2025 © TheFuZeeXD All rights reserved.
@@ -14,26 +12,26 @@ import { Background } from "../../App.jsx";
 export default function Footer() {
   const [isTheme, set] = useState(configSettings.themeColor);
 
-function changeTheme() {
-        set(isTheme == "Light" ? "Hight" : "Light");
-        localStorage.setItem('Theme-color', isTheme);
-        location.reload();
-        // localStorage.getItem("Theme-color") == "Hight" ? Background.src = Banner : Background.src = BannerLight;
-      };
+  function changeTheme() {
+    set(isTheme == "Light" ? "Hight" : "Light");
+    localStorage.setItem('Theme-color', isTheme);
+    location.reload();
+    // localStorage.getItem("Theme-color") == "Hight" ? Background.src = Banner : Background.src = BannerLight;
+  };
 
-    useEffect(() => {
-        set(isTheme == "Light" ? "Hight" : "Light");
-        localStorage.setItem('Theme-color', isTheme);
-    }, []);
-      
-    return (
-        <footer>
-            <h2>2025 © TheFuZeeXD All rights reserved.</h2>
-            <a href="https://github.com/TheFuZeeXD/thefuzeexd.ru">Source❤️Code</a>
-            <div id="ImgBox">
-            <img onClick={changeTheme} src={localStorage.getItem("Theme-color") == "Hight" ? Mon : Sun}></img>
-            </div> 
-            </footer>
-    )
+  useEffect(() => {
+    set(isTheme == "Light" ? "Hight" : "Light");
+    localStorage.setItem('Theme-color', isTheme);
+  }, []);
+
+  return (
+    <footer>
+      <h2>{LanguagePack.footer.title}</h2>
+      <a href="https://github.com/TheFuZeeXD/thefuzeexd.ru">Source❤️Code</a>
+      <div id="ImgBox">
+        <img onClick={changeTheme} src={localStorage.getItem("Theme-color") == "Hight" ? Mon : Sun}></img>
+      </div>
+    </footer>
+  )
 }
 
