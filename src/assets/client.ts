ec750612b7abb20d3e32f8074fbbc929e644d0c9
fuzeexd <fuzeexd@thefuzeexd.ru>
2025-07-07 10:10:35 +0300

    Update client.ts

--- src/assets/client.ts
+++ src/assets/client.ts
@@ -1,9 +1,25 @@
 // Scripts
+import { theme } from "./theme";
+import Header from "../components/header/Header"
+import Footer from "../components/footer/Footer"
+import Code from "../components/code/Code"
+const themeColor: string = localStorage.getItem("Theme-color") || "undefined";
 
-const themeColor: string = localStorage.getItem("Theme-color") || "Light";
+export const HTMLDoom: object = {
+    Header,
+    Footer,
+    Code
+}
 
-const configSettings: object = {
+export const configSettings: object = {
     themeColor
 };
 
-export default configSettings;
+
+if (localStorage.getItem("Theme-color") == null || localStorage.getItem("Theme-color") == "undefined") {
+    if (theme == "Light") {
+        localStorage.setItem("Theme-color", "Light")
+    } else if (theme == "Dark") {
+        localStorage.setItem("Theme-color", "Hight")
+    }
+}
