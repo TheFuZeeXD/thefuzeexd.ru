70b39d519e3afc34ef943359d0e1c651edea904b
fuzeexd <fuzeexd@thefuzeexd.ru>
2025-07-21 13:09:04 +0300

    Update theme.ts

--- src/assets/theme.ts
+++ src/assets/theme.ts
@@ -4,11 +4,15 @@ const not_theme_system = window.matchMedia('(prefers-color-scheme: no-preference
 
 let defaultTheme;
 if (dark_theme_system == true) {
+    if (!localStorage.getItem("Theme-color")) { localStorage.setItem("Theme-color", "Hight"); }
     defaultTheme = "Dark";
 } else if (light_theme_system == true) {
+    if (!localStorage.getItem("Theme-color")) { localStorage.setItem("Theme-color", "Light"); }
     defaultTheme = "Light";
 } else {
+    if (!localStorage.getItem("Theme-color")) { localStorage.setItem("Theme-color", "Light"); }
     defaultTheme = "Light";
 }
+
 
 export const theme = defaultTheme; 
