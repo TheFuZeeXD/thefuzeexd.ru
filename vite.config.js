c38a260900bc4869f2ef49ef9da2b457d9239459
TheFuZeeXD <fuzeexd@thefuzeexd.ru>
2025-08-14 20:26:52 +0300

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
 import php from 'vite-plugin-php'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
+    react(),
     php()
   ],
   base: '/',
@@ -14,14 +16,16 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
       output: {
         assetFileNames: 'assets/[name].[hash][extname]',
         entryFileNames: 'assets/[name].[hash].js'
       }
     }
   },
-  // Добавляем поддержку HTML импортов
-  optimizeDeps: {
-    include: ['*.html']
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
